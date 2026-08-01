b5e5b155b475891a6d48e2d15923e9309cce72a7
Senbagarajan.S <senbagarajan2807@gmail.com>
2026-08-01 19:06:45 +0530

    Time: 3 ms (78.06%), Space: 46.3 MB (75.75%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int romanToInt(String s) {
+
+        int[] value = new int[128];
+
+        value['I'] = 1;
+        value['V'] = 5;
+        value['X'] = 10;
+        value['L'] = 50;
+        value['C'] = 100;
+        value['D'] = 500;
+        value['M'] = 1000;
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (i < s.length() - 1 &&
+                value[s.charAt(i)] < value[s.charAt(i + 1)]) {
+
+                ans -= value[s.charAt(i)];
+            } else {
+                ans += value[s.charAt(i)];
+            }
+        }
+
+        return ans;
+    }
+}
